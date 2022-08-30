6a5e82ad696b57aa40d90d6c9a8d99ddee031827
Maryia Kashkan <mary.kashkan@gmail.com>
2022-08-31 01:49:32 +0300

    feat: add animation

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -30,4 +30,83 @@
   }
 
   collapsibleButton.addEventListener("click", makeCollapse);
+
+  var buttonColorAnimation = [
+    {
+      color: "#000000",
+      width: "340px",
+      letterSpacing: "3px",
+      height: "50px",
+      fontSize: "18px",
+      fontWeight: "bold",
+      boxShadow: "0 0 40px rgba(0,0,0)",
+    },
+    {
+      color: "#000000",
+      width: "340px",
+      letterSpacing: "3px",
+      height: "50px",
+      fontSize: "24px",
+      fontWeight: "bold",
+      boxShadow: "0 0 40px rgba(0,0,0)",
+    },
+    {
+      color: "#000000",
+      width: "340px",
+      letterSpacing: "3px",
+      height: "50px",
+      fontSize: "18px",
+      fontWeight: "bold",
+      boxShadow: "0 0 40px rgba(0,0,0)",
+    },
+  ];
+
+  var buttonTiming = {
+    duration: 4000,
+    iterations: Infinity,
+  };
+
+  collapsibleButton.animate(buttonColorAnimation, buttonTiming);
+
+  var collapsContainerAnimation = [
+    { marginTop: "400px", marginLeft: "700px" },
+    { marginTop: "400px", marginLeft: "700px" },
+  ];
+
+  var collapsContainerTiming = {
+    duration: 3000,
+    iterations: Infinity,
+  };
+
+  document
+    .querySelector(".collapsible")
+    .animate(collapsContainerAnimation, collapsContainerTiming);
+
+  var collapsContentAnimation = [
+    {
+      color: "#8100DB",
+      fontSize: "30px",
+      marginLeft: "-400px",
+      marginTop: "100px",
+    },
+    {
+      color: "#ffffff",
+      fontSize: "45px",
+      marginLeft: "-400px",
+      marginTop: "100px",
+    },
+    {
+      color: "#00DB68",
+      fontSize: "30px",
+      marginLeft: "-400px",
+      marginTop: "100px",
+    },
+  ];
+
+  var collapsContentTiming = {
+    duration: 12000,
+    iterations: Infinity,
+  };
+
+  collapsibleContent.animate(collapsContentAnimation, collapsContentTiming);
 })();
